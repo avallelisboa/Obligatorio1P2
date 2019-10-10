c6cb0a13a2d13108c820426f0c9be022cf0512a1
Andrés <avallelisboagmail.com>
2019-10-10 16:46:34 -0300

    addToPurchase

--- ShopSystem/Purchase.cs
+++ ShopSystem/Purchase.cs
@@ -7,20 +7,49 @@ namespace ShopSystem
     class Purchase
     {
         private Client client;
-        List<Product> productsToBuy = new List<Product>();
+        private List<Product> productsToBuy = new List<Product>();
+        private int totalPrice = 0;
+        private Purchase(Client client){ this.client = client;}
 
-        private Purchase(Client client){ this.client = client}
+        private int calculatePurchasePrice()
+        {
+            return totalPrice;
+        }
 
         public static Purchase getPurchase(Client client)
         {
             return new Purchase(client);
         }
 
-        public bool buy()
+        public string buy()
         {
-            throw new NotImplementedException();
+            
         }
 
-        
+        public string addToPurchase(ProductStock productStock, Product product, int quantity)
+        {
+            bool wasFounded = false;
+            List<Product> products = productStock.ProductsList;
+            int productsNumber = productStock.ProductsQuantity;
+            int productsAdded = 0;
+            if(quantity <= productsNumber)
+            {
+                for(int i=0;i<productsNumber;i++)
+                {
+                    if(products[i] == product)
+                    {
+                        productsToBuy.Add(products[i]);
+                        productsAdded++;
+                        totalPrice += products[i].price;
+                    }
+                    if(productsAdded == quantity) break;
+                }
+                return "The products were added successfully";
+            }
+            else
+            {
+                return "The are not enough products in stock to add";
+            }           
+        }
     }
 }
